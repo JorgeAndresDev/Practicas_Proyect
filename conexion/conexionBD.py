98a1c6da1d37a2d08a9c31d7d0d909336b38410e
davidlopez61 <davidjos61@gmail.com>
2025-02-28 10:07:11 -0500

    Primer commit: Subiendo mi proyecto

---
+++ conexion/conexionBD.py
@@ -0,0 +1,25 @@
+
+
+# Importando Libreria mysql.connector para conectar Python con MySQL
+import mysql.connector
+
+
+def connectionBD():
+    try:
+        # connection = mysql.connector.connect(
+        connection = mysql.connector.connect(
+    host="localhost",
+    port=3306,  # Cambia esto si usas un puerto diferente
+    user="root",
+    passwd="1234",
+    database="app_empresa_bd",
+    charset='utf8mb4',
+    collation='utf8mb4_unicode_ci',
+    raise_on_warnings=True
+)
+        if connection.is_connected():
+            print("Conexión exitosa a la BD")
+            return connection
+
+    except mysql.connector.Error as error:
+        print(f"No se pudo conectar: {error}")
