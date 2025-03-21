9c52982d0a8a754b6add57895128d361c98676fe
Jorge Gomez <jorgeandresg1207@gmail.com>
2025-03-21 15:01:04 -0500

    Script db

---
+++ BD/app_empresa_bd_users.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: app_empresa_bd
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name_surname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pass_user` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created_user` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (4,'Juan perez','juan@perez.com','scrypt:32768:8:1$RAo8NZGQTnUvKnog$f9586777bcc9f3d3ed3979f7e252f2ed84dbc11b71df647a0db42ce76b7a6180db8dbdb54d1fd27ecae5d0f44f0fa41c481dd663d7025fc1613eef8c24093695','2025-03-19 19:14:28'),(5,'Yefri','yefri@yefri.com','scrypt:32768:8:1$RA9kX0qSgU8PwvsG$c235e46e6dd15a519a4b47aaa917beef7801367961048c352311243024320dc1dcb00ebc48636d950c2bbba54514e0e22626dd2c919d0ddca6068a71e901ae70','2025-03-19 19:15:08'),(6,'Jorge andres ','jorge@andres.com','scrypt:32768:8:1$XSW8NFwBsihKxqf0$ab38a46158e72aa56f1208389b5430f1e71e9481c7b1386c6c6d1a4076ebaef71d60c1ea067ab4e9c3f3b767bf70d4b82b1436cd466287ebf8f05b47acc19314','2025-03-19 19:39:03'),(7,'Wilson salas','salaswilson626@gmail.com','scrypt:32768:8:1$jJAP4JzaI5JdRqFR$87b59009743f893dbb8a4ccff4014bfaf2ec23d92e30e3be396293ad93393af150fbec341641c1900422095c1ad13e3c78ce49cf482a3c6aeb8c5b859688197f','2025-03-20 15:29:24'),(8,'carlos','carlos@gmail.com','scrypt:32768:8:1$SSGRl5EpmTFrps0Q$bf6db4e426c57189cf5ee09fd97c2ec819a603da441a61b4f62630577de3f360e93fc23ca4a6ff1252e99a9a9eb0cf456f34764fa3a0223e1d57c2404628bd0f','2025-03-20 15:31:29'),(9,'YEFRI','YEFRIBAR@GMAIL.com','scrypt:32768:8:1$ntwWroepYrxShIZl$1e9a6778f2ac4964de8cf8ef9f0ef1d1e3ea6d3eb862c93f4cbe1de1036c0829a52286fc085415db1411ef157e14cb76b56f0ce42a1351fafac81111a0cdd6a6','2025-03-20 15:37:01'),(10,'Albeiro padilla','ap@gmail.com','scrypt:32768:8:1$VNCHJMYtdR5TYlv2$2aaf80ba7d5d80cbaa5ec40a610ce9b856d82f417a71a54cc5b03763202df8ae866cd4164a164200d01cab507e6232e44579ecf053d18735ec3d75166bf7f0a4','2025-03-20 15:43:56'),(11,'Jorge Andres Gomez Diaz','JorgeGomez@gmail.com','scrypt:32768:8:1$dmhjZVvQc1tAojmR$301cf4bc608910e1a41fe64acd66d29d666e100891246f2eb33262163c5cd9e5812093fe6e77002038cdf6e3a6c09bd7555aa645f729523243f9c030c6a1026b','2025-03-20 17:37:40');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-21 14:58:25
